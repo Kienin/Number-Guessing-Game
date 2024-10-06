c8517026e7f8bb863441267a5931a679bfe24230
Kevin Dacanay <136418810+Kienin@users.noreply.github.com>
2024-10-05 20:17:40 -0500

    Committing Python Number Game

--- 1.0 Project - Number Guessing Game.py
+++ 1.0 Project - Number Guessing Game.py
@@ -1,47 +1,57 @@
 from random import randint
 
+# Get the user's name and greet them
 name = input("What is your name?: ")
 print(f"Hello {name.capitalize()}! Let's play guess the number!")
 
+# Get the minimum and maximum values for the guessing range
 while True:
     try:
         min_val = int(input("Enter the minimum number: "))
-        break
+        break  # Exit the loop if the input is valid
     except ValueError:
         print("Please enter an integer value.\n")
 
 while True:
     try:
         max_val = int(input("Enter the maximum number: "))
-        break
+        break  # Exit the loop if the input is valid
     except ValueError:
         print("Please enter an integer value.\n")
 
+# Get the number of tries allowed
 while True:
     try:
         num_tries = int(input("How many times would you like?: "))
-        break
+        break  # Exit the loop if the input is valid
     except ValueError:
         print("Please enter an integer value.\n")
 
+# Generate a random number within the specified range
 number = randint(min_val, max_val)
 
+# Initialize the count of attempts
 count = 1
 
+# Main game loop
 while count <= num_tries:
+    # Get the user's guess
     guess = int(input("\nGuess the number: "))
 
+    # Check if the guess is correct
     if guess == number:
         print(f"You guessed the number in {count} tries. Congratulations!")
-        break
+        break  # Exit the loop if the guess is correct
 
+    # Provide feedback if the guess is incorrect
     elif guess < number:
         print("Your guess is lower than the number.")
-
     else:
         print("Your guess is higher than the number")
 
-    count +=1
+    # Increment the count of attempts
+    count += 1
 
+# Check if the user ran out of tries
 if count > num_tries:
     print(f"You couldn't guess the number in {num_tries} tries. The number was {number}.")
